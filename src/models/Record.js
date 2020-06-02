edcd219ec326efce4c358150919d7d03e6f0a0cf
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 12:39:20 +0100

    create model schema for mongoose

--- src/models/Record.js
+++ src/models/Record.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let record = new Schema(
+    {
+        key: {
+            type: String
+        },
+        createdAt: {
+            type: Date
+        },
+        counts: {
+            type: Array
+        }
+    },
+    { collection: "records" }
+);
+
+module.exports = mongoose.model("Record", record);
