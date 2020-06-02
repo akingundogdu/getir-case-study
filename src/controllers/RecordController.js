bbfd6f8492764bb83012a4e474a51905e0f0dfcb
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 14:42:35 +0100

    complete controller logic

--- src/controllers/RecordController.js
+++ src/controllers/RecordController.js
@@ -1,1 +1,39 @@
-"use strict";
+const {body, validationResult} = require('express-validator')
+const ResponseHelper = require('../infrastructure/ResponseHelper')
+const RecordService = require('../services/RecordService')
+
+/**
+ * @function records
+ * @description {post} /api/records action method of RecordController
+ *
+ * @param  {HttpRequest} [request] Http Request
+ * @param  {HttpResponse} [response] Http Response
+ *
+ * @return HttpResponse
+ */
+exports.records = async function (request, response) {
+    try {
+        const validate = validationResult(request);
+        if (!validate.isEmpty()) {
+            return await ResponseHelper.notValidated(response, validate.array())
+        }
+
+        const parameters = {startDate, endDate, minCount, maxCount} = request.body
+        const pagination = {page, limit} = request.query
+        const records = await RecordService.records(parameters, pagination.page, pagination.limit)
+
+        return ResponseHelper.ok(response, records)
+    } catch (exception) {
+        return ResponseHelper.notOk(response, exception.statusCode, exception.message);
+    }
+}
+
+
+exports.validate = (method) => {
+    return [
+        body('startDate').exists().isISO8601().toDate(),
+        body('endDate').exists().isISO8601().toDate(),
+        body('minCount').exists().isInt(),
+        body('maxCount').exists().isInt()
+    ]
+}
