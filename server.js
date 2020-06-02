8222581c337fb32f850471f6eb9756c19aee443a
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 11:52:18 +0100

    make hello world example for check everything is ok

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 "use strict";
+require('dotenv').config();
+const express = require('express');
+const route = require('./src/routes/Record');
+
+const app = express();
+app.use(express.json());
+app.use(route);
+
+module.exports = app;
--- src/routes/Record.js
+++ src/routes/Record.js
@@ -1,1 +1,11 @@
 "use strict";
+
+
+const express = require('express');
+const router = express.Router();
+
+router.get("/hello", (req, res) => {
+    res.status(200).send("Hello World!");
+});
+
+module.exports = router;
