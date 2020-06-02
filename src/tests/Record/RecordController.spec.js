66223b137944e318633cfbf9f32fa199e4c13556
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 11:58:05 +0100

    write empty integration test use cases for cover RecordController

--- src/tests/Record/RecordController.spec.js
+++ src/tests/Record/RecordController.spec.js
@@ -1,1 +1,21 @@
 "use strict";
+
+describe('POST /api/records', () => {
+    it('should return correct response structure', async done => {})
+
+    it('should give not found error when make post /api/recordss', async done => {})
+
+    it('should give validation error with 412 when not pass any parameter', async done => {})
+
+    it('should give validation error with 412 when not pass startDate parameter', async done => {})
+
+    it('should give validation error with 412 when not pass endDate parameter', async done => {})
+
+    it('should give validation error with 412 when not pass minCount parameter', async done => {})
+
+    it('should give validation error with 412 when not pass maxCount parameter', async done => {})
+
+    it('should give validation error with 412 when pass wrong date format', async done => {})
+
+    it('should give validation error with 412 when pass dd-mm-yyyy date format', async done => {})
+})
