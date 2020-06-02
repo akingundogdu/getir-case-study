8925dcbebd4e9e3c4f6b28eced948fc24aed470b
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 12:05:20 +0100

    create request/response helper for more code readability

---
+++ src/infrastructure/ResponseHelper.js
@@ -0,0 +1,21 @@
+"use strict";
+
+exports.ok = async function (response) {
+    return response;
+}
+
+exports.notOk = async function (response) {
+    return response;
+}
+
+exports.created = async function (response) {
+    return response;
+}
+
+exports.notValidated = async function (response) {
+    return response;
+}
+
+exports.notFound = async function (response) {
+    return response
+}
