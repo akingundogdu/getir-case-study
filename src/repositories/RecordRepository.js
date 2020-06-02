8304f0dc0b15344ffddf4a08fdb67e9dad9d25ee
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 12:40:30 +0100

    implement records repository

--- src/repositories/RecordRepository.js
+++ src/repositories/RecordRepository.js
@@ -1,1 +1,41 @@
 "use strict";
+const Record = require('../models/Record');
+
+
+/**
+ * @function All
+ * @description Retrieve record models from db
+ *
+ * @param  {array} [query] Query parameters
+ * @param  {Int} [page] Page
+ * @param  {Int} [limit] Limit
+ * @param  {Int} [maxCount] Maximum Count
+ *
+ * @return Record[]
+ */
+exports.all = async function (query, page, limit) {
+    const {startDate, endDate, minCount, maxCount} = query;
+    const record = await Record.aggregate([
+        {
+            $match: {
+                createdAt: {
+                    $gte: new Date(startDate),
+                    $lte: new Date(endDate)
+                },
+                counts: {
+                    $gt: minCount,
+                    $lt: maxCount
+                }
+            }
+        },
+        {
+            $project: {
+                _id: 0,
+                key: "$key",
+                createdAt: "$createdAt",
+                totalCount: {$sum: "$counts"}
+            }
+        }
+    ]);
+    return record;
+};
