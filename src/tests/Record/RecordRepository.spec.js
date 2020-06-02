fec793c728dabc51052aaa6dab3c3947e7d4831f
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 12:40:45 +0100

    complete records repository unit test

--- src/tests/Record/RecordRepository.spec.js
+++ src/tests/Record/RecordRepository.spec.js
@@ -1,5 +1,45 @@
 "use strict";
 
-describe('Record Repository', () =>{
-    it('should return record list', function () {});
+const DB = require('../util/MongoDbFaker');
+const recordRepository = require('../../repositories/RecordRepository');
+const Record = require('../../models/Record');
+
+beforeEach(async () => DB.connect())
+afterEach(async () => DB.disconnect())
+
+describe('Record Repository', () => {
+    it('should return record list', async () =>{
+        await Record.insertMany([
+            {
+                "key": "TAKwGc6Jr4i8Z487",
+                "createdAt": "2017-01-28T01:22:14.398Z",
+                "counts": [2800]
+            },
+            {
+                "key": "NAeQ8eX7e5TEg7oH",
+                "createdAt": "2017-01-27T08:19:14.135Z",
+                "counts": [2900]
+            }
+        ])
+        const records = await recordRepository.all({
+            "startDate": "2016-01-26",
+            "endDate": "2018-02-02",
+            "minCount": 2700,
+            "maxCount": 3000
+        })
+
+        expect(await Record.countDocuments()).toBe(2)
+        expect(records).toStrictEqual([
+            {
+                key: 'TAKwGc6Jr4i8Z487',
+                createdAt: new Date('2017-01-28T01:22:14.398Z'),
+                totalCount: 2800
+            },
+            {
+                key: 'NAeQ8eX7e5TEg7oH',
+                createdAt: new Date('2017-01-27T08:19:14.135Z'),
+                totalCount: 2900
+            }
+        ])
+    });
 })
