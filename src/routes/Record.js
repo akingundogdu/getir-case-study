a9fda6b426a2c1dd041e2ce7a249b375b242403a
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 14:43:54 +0100

    create and implement route of POST api/routes

--- src/routes/Record.js
+++ src/routes/Record.js
@@ -1,11 +1,23 @@
-"use strict";
-
+"use strict"
 
 const express = require('express');
 const router = express.Router();
 
-router.get("/hello", (req, res) => {
-    res.status(200).send("Hello World!");
-});
+const RecordController = require('../controllers/RecordController')
+
+/**
+ * This function comment is parsed by doctrine
+ * @route POST /api/records
+ * @group records - Operations about records model
+ * @param  {Date} [startDate] Start Date
+ * @param  {Date} [endDate] End Date
+ * @param  {Int} [minCount] Minimum Count
+ * @param  {Int} [maxCount] Maximum Count
+ * @returns {object} 200 - An array of record info
+ * @returns {Error}  default - Unexpected error
+ */
+router.post('/api/records',
+    RecordController.validate('records'),
+    RecordController.records);
 
 module.exports = router;
