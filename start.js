51a169600eb2cbfe435d79e1bfc0d4bf827b5966
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 12:22:39 +0100

    implement into start.js, MongoDB database connection established successfully

--- start.js
+++ start.js
@@ -2,5 +2,7 @@
 
 
 const app = require('./server');
+const mongoDB = require('./src/database/Mongo');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server listening at port ${PORT}`));
+mongoDB.connect();
