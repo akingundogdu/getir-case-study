c7e27fdec65f05d13df4eeaf67662afe20a0545b
akingundogdu <akin-gundogdu@hotmail.com>
2020-06-02 12:42:17 +0100

    implement records service

--- src/services/RecordService.js
+++ src/services/RecordService.js
@@ -1,1 +1,16 @@
-"use strict";
+const RecordRepository = require('../repositories/RecordRepository');
+
+/**
+ * @function Records
+ * @description Retrieve record models from repository layer
+ *
+ * @param  {array} [query] Query parameters
+ * @param  {Int} [page] Page
+ * @param  {Int} [limit] Limit
+ * @param  {Int} [maxCount] Maximum Count
+ *
+ * @return Record[]
+ */
+exports.records = async function (query, page, limit) {
+    return RecordRepository.all(query, page, limit);
+}
